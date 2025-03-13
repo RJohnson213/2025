9c86f97ef01d93659e73d7c3099b355601037cc2
RJohnson213 <a02305586@usu.edu>
2025-03-13 14:28:04 -0600

    updated to the code will build

--- src/Control.hh
+++ src/Control.hh
@@ -20,7 +20,7 @@ float predictAltitude(float height, float velocity);
 #define CONTROL_P       (-0.6f)
 #define CONTROL_I       (-0.005f)
 #define CONTROL_BIAS    (0.00f) //Yes this should be negative
-#define UPPER_CONTROL_SATURATION (M_PI_2)
+#define UPPER_CONTROL_SATURATION (140 * M_PI / 180)
  
 extern float integratorState;
 float getControl(float desired, float predicted, float dt);
